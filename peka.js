010e23f3bee8aa06c8f962d7fe72c5f70fb43f82
Emil Johansson <emiljohansson.se@gmail.com>
2014-08-26 18:54:21 -0500

    add touchend and click event

--- peka.js
+++ peka.js
@@ -14,6 +14,16 @@
         if (typeof window.bowser === 'undefined') {
             throw 'peka.js is requires the Bowser library. window.bowser was undefined';
         }
+        this._elem = elem;
+    }
+
+    Peka.prototype.tap = function(listener) {
+        if (bowser.mobile === true ||bowser.tablet === true) {
+            this._elem.addEventListener('touchend', listener);
+        }
+        else {
+            this._elem.addEventListener('click', listener);
+        }
     }
 
     window.Peka = Peka;
